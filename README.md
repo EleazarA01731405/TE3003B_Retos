8e58b59af8fd708412866847662f28bf9905168c
Eleazar <a01731405@itesm.mx>
2025-03-28 16:41:40 -0600

    Setup.py y package.xml acabados

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TE3003B_Retos
+
+el arick es puto y por eso nos tardamos mas de los que necesitabamos
--- puzzlebot_sim/setup.py
+++ puzzlebot_sim/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'puzzlebot_sim'
 
@@ -6,17 +8,17 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+    data_files=[('share/ament_index/resource_index/packages',
+                 ['resource/' + package_name]),
+                 ('share/' + package_name,['package.xml']),
+                 (os.path.join('share', package_name, 'launch'),glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='eleazar',
     maintainer_email='a01731405@itesm.mx',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Reto semana 1',
+    license='Apache 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
