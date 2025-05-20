5f0b308d5650beb854fd2829ef0ecd461085bd1e
Eleazar <a01731405@tec.mx>
2025-05-20 17:08:31 -0600

    Primer avance wall follower, falta prueba

--- reto_final_real/wall_follower.py
+++ reto_final_real/wall_follower.py
@@ -14,24 +14,39 @@ class WallFollower(Node):
             self.declare_parameter('use_sim_time', True)
 
         # Subscriber to lidar topic for obstacle detection
-        self.lidar_subscriber = self.create_subscription(LaserScan,'/scan',self.lidar_callback,10)
+        self.lidar_subscriber = self.create_subscription(
+            LaserScan,              # Message type  
+            '/scan',                # Topic name
+            self.lidar_callback,    # Callback function
+            10                      # QoS (queue size)
+        )
+        
         # Publiser to cmd_vel for robot movement and reaction
-        self_vel_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.cmd_vel_publisher = self.create_publisher(
+            Twist,                  # Message type
+            '/cmd_vel',             # Topic name
+            10                      # QoS (queue size)
+        )
 
         # Globarl variables
 
         # Distance threshold to detect obstacles (meters) [min]
-        self.min_threshold = 0.10
+        self.min_threshold = 0.15
         # Distance threshold to detect obstacles (meters) [max]
-        self.max_threshold = 0.15
+        self.max_threshold = 0.25
+        # Distance threshold aid to detect obstacle's corners (meters) [max]
+        self.back_max_threshold = 0.50
 
         # Lidar data reading variables ranges
         self.min_dis_f = 0
-        self.min_dis_l = 0
         self.min_dis_r = 0
+        self.min_dis_b = 0
 
         # Timer callback
         self.timer = self.create_timer(0.1, self.loop_callback)
+
+        # Obstacle found control variable
+        self.obstacle_found = False
 
     def lidar_callback(self, msg):
         # Extract the first 20 and last 20 values from the front reading ranges array
@@ -42,22 +57,76 @@ class WallFollower(Node):
         front = front1 + front2
 
         # Extract the left and right values from the ranges array
-        left = msg.ranges[70:110]
         right = msg.ranges[250:290]
+        #left = msg.ranges[70:110]
 
+        # Extract the back values from the ranges array
+        back = msg.ranges[180:220]
         # Filter out inf values and NaN values
         val_readings_f = [x for x in front if x != float('inf') and not math.isnan(x)]
-        val_readings_l = [x for x in left if x != float('inf') and not math.isnan(x)]
+        val_readings_l = [x for x in back if x != float('inf') and not math.isnan(x)]
         val_readings_r = [x for x in right if x != float('inf') and not math.isnan(x)]
         
+    def loop_callback(self):
+        
+        # Check if there is and obstacle in front (fisrt contact)
+        if (self.obstacle_found == False) and (self.min_threshold < self.min_dis_f < self.max_threshold):
+            self.turn_l()
+            self.obstacle_found = True
+            self.get_logger().info('Obstacle detected FRB[  True  |  False  |  False  ], turning left')
+        
+        # Check if there is an obstacle on the right side, but not  on the front (Following object)
+        elif (self.obstacle_found == True) and (self.min.dis_f > self.max_threshold) and  (self.min_threshold < self.min_dis_r < self.max_threshold):
+            self.move_forward()
+            self.get_logger().info('Obstacle detected FRB[  False  |  False  |  True  ], moving forward')
+        
+        # Check if there is an obstacle on the right side and on the front (Following object)
+        elif (self.obstacle_found == True) and (self.min_threshold < self.min_dis_f < self.max_threshold) and  (self.min_dis_r < self.max_threshold):
+            self.turn_l()
+            self.get_logger().info('Obstacle detected FRB[  True  |  True  |  False  ], turning left')
+        
+        # Check for corners of object found (Following object)
+        elif (self.obstacle_found == True) and (self.min_dis_f > self.max_threshold) and  (self.min_dis_r > self.max_threshold) and (self.min_threshold < self.min_dis_b < self.back_max_threshold):
+            self.turn_r()
+            self.get_logger().info('Obstacle corner detected FRB[  False  |  False  |  True  ], turning right')
+
+        # Object clear (Moving foward)
+        else :
+            self.move_forward()
+            self.obstacle_found = False
+            self.get_logger().info('Obstacle cleared, moving forward')
+
+
+    # Move the robot forward        
+    def move_forward(self):
+        twist = Twist()
+        twist.linear.x = 0.15  # Move forward at 0.2 m/s
+        twist.angular.z = 0.0
+        self.cmd_vel_publisher.publish(twist)
+
+    # Turn the robot to the right
+    def turn_r(self):
+        twist = Twist()
+        twist.linear.x = 0.1
+        twist.angular.z = -0.15  # Turn at 0.5 rad/s
+        self.cmd_vel_publisher.publish(twist)
+
+    # Turn the robot to the left
+    def turn_l(self):
+        twist = Twist()
+        twist.linear.x = 0.1
+        twist.angular.z = 0.15  # Turn at -0.5 rad/s
+        self.cmd_vel_publisher.publish(twist)
+    
+
+def main(args=None):
+    rclpy.init(args=args)
+    wall_follower = WallFollower()
+    rclpy.spin(wall_follower)
+    wall_follower.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
         
         
-        
-        #Notas para read me file
-
-        # Se debe de conectar el LiDar despues de que haya boteado la jetson
-
-        # Velocidades lineales y angulares no deben de pasar de 0.2 en cmd_vel
-
-        # Se de be de lanzar el agente de micro_ros con el puerto especificado ttyUSB1
-        #ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB1
